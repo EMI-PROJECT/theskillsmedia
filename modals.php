54043a331fc2f5717ff32a2192cfc5fa6a31639f
Furaha Emile <furahaemire5@gmail.com>
2024-08-09 11:47:22 -0700

    new files

---
+++ modals.php
@@ -0,0 +1,22 @@
+
+<!-- home modal -->
+<div class="modal fade" id="home" tabindex="-1" aria-labelledby="exampleModalCenteredScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalCenteredScrollableTitle">Muraho neza!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- start -->
+      <p> Mbere yo gutangira guhitamo amasomo wifuza urabanza ufungure konti.</p>
+         <p>|| <b>The skills Media</b></p>
+    <!-- end -->
+      </div>
+      <div class="modal-footer">
+      <button type="button" data-bs-toggle='modal' data-bs-target='#login'  class="btn btn-primary">Injira</button>
+      <button type="button" data-bs-toggle='modal' data-bs-target='#register'  class="btn btn-primary">Fungura konti</button>
+      </div>
+    </div>
+  </div>
+</div>
