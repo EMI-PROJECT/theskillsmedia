60016071a3979f1e38ab9b7673bcd2beaab250d0
Furaha Emile <furahaemire5@gmail.com>
2024-08-12 15:10:23 -0700

    some changes

--- module/user/dashboard.php
+++ module/user/dashboard.php
@@ -1,78 +1,72 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>The skillsmedia // Uhawe ikaze</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style/style.css">
-  <!-- linking google fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uhawe ikaze kurubuga rwigisha amasomo // THE SKILLS MEDIA</title>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style/dashboard.css">
+<!-- GOOGLE FONTS -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
 <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- linking bootstrap framework cdn link -->
+
+<!-- bootstrap cdn -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </head>
-
 <body>
-<!-- include importance files -->
-<?php include('include/header.php');?>
+<!-- include some importance files -->
 <?php include('modals.php');?>
+
+    
+<!-- welcome user // this item will display to all pages -->
+ <br><br>
+<div class="alert alert-warning alert-dismissible fade show center"  role="alert">
+          Hi.. <b> Furaha Emile </b>,Uhawe ikaze kuri the skills media    
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+<!-- here is course card -->
  
-<!-- section1: intoduction -->
-<br><br>
-<div class="content">
-<p class="h2">Tangira Kongera ubumenyi Uri murugo</p>
-  <p class="lead"> Tangira kwiga ndetse kongera ubumenyi bushya ukoresheje iyi app. Ushobora kandi nawe gushyiraho
-    amasomo muburyo bworoshye kandi bwihuse
-  </p>
-  <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
-</div>
-<br>
-<br>
-
-
-<!-- section 2: course cards -->
-
-
-<p class="h4 content">Hitamo isomo wifuza kwiga:</p><br>
+<p class="h4 content center">Hitamo isomo wifuza kwiga:</p><br>
 <div class="todo">
     <div class="rowc">
        <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
        <p>Gukora website ukoresheje html na csss</p>
-       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
+       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#coursedetail'>Tangira kwiga</button>
        <br> <br>
     </div> 
 
     <div class="rowc">
        <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
        <p>Gukora website ukoresheje html na csss</p>
-       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
+       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#coursedetail'>Tangira kwiga</button>
        <br> <br>
     </div> 
-
-    
 
 
     <div class="rowc">
        <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
        <p>Gukora website ukoresheje html na csss</p>
-       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
-       <br> <br>
-    </div> 
-
-    <div class="rowc">
-       <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
-       <p>Gukora website ukoresheje html na csss</p>
-       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
+       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#coursedetail'>Tangira kwiga</button>
        <br> <br>
     </div> 
 
     <div class="rowc">
        <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
        <p>Gukora website ukoresheje html na csss</p>
-       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#home'>Tangira kwiga</button>
+       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#coursedetailme'>Tangira kwiga</button>
+       <br> <br>
+    </div> 
+
+    <div class="rowc">
+       <img src="https://th.bing.com/th/id/OIP.94dELyKmnTGlEbXYA5YpjQHaD4?rs=1&pid=ImgDetMain" alt="">
+       <p>Gukora website ukoresheje html na csss</p>
+       <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#coursedetail'>Tangira kwiga</button>
        <br> <br>
     </div> 
 </div>
@@ -80,9 +74,6 @@
 
 
 
-
-
-<?php include('include/footer.php'); ?>
 
 </body>
 </html>
