f4b9caf0619350df384a1fdd15217afb146ae565
Niyonsavye <emmaniyoinfo@gmail.com>
2024-08-14 09:47:27 +0200

    skills media project]'

--- modals.php
+++ modals.php
@@ -1,3 +1,6 @@
+<!-- <link rel="stylesheet" href="style/style1.css"> -->
+<script src="../custom-scripts.js" defer></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <!-- home modal -->
 <div class="modal fade" id="home" tabindex="-1" aria-labelledby="exampleModalCenteredScrollableTitle" aria-hidden="true">
@@ -10,13 +13,114 @@
       <div class="modal-body">
         <!-- start -->
       <p> Mbere yo gutangira guhitamo amasomo wifuza urabanza ufungure konti.</p>
-         <p>|| <b>The skills Media</b></p>
+         <p>|| <b>The skills Media</b></p>    
+
+
+
+          
     <!-- end -->
       </div>
       <div class="modal-footer">
-      <button type="button" data-bs-toggle='modal' data-bs-target='#login'  class="btn btn-primary">Injira</button>
-      <button type="button" data-bs-toggle='modal' data-bs-target='#register'  class="btn btn-primary">Fungura konti</button>
+      <div class="modal-footer">
+  <button type="button" data-bs-toggle="modal" data-bs-target="#login" class="btn btn-primary">Injira</button>
+  <button type="button" data-bs-toggle="modal" data-bs-target="#signup" class="btn btn-primary">Fungura konti</button>
+</div> </div>
+    </div>
+  </div>
+</div>         
+
+
+<section class="wrapper">
+
+<!--login    modal -->
+<div class="modal fade" id="login" tabindex="-1" aria-labelledby="exampleModalCenteredScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+    <!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content=" Today in this blog you will learn how to create a responsive Login & Registration Form in HTML CSS & JavaScript. The blog will cover everything from the basics of creating a Login & Registration in HTML, to styling it with CSS and adding with JavaScript." />
+    <meta
+      name="keywords"
+      content=" 
+ Animated Login & Registration Form,Form Design,HTML and CSS,HTML CSS JavaScript,login & registration form,login & signup form,Login Form Design,registration form,Signup Form,HTML,CSS,JavaScript,
+"
+    />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title></title>
+
+    <script src="../custom-scripts.js" defer></script>
+  </head>
+  <body1>
+    <section class="wrapper1">
+      
+     <div class="a">
+      <div class="form login">
+        <header>Kwinjira</header>
+        <form action="#">
+          <input type="text" placeholder="Injiza Email Yawe" required /><br><br>
+          <input type="password" placeholder="Injiza Kbanga" required /><br><br>
+          <a href="#">Wibagiye Kabanga?</a>
+          <input type="submit" value="Injira" />
+        </form>
       </div>
+      </div>
+      
+    </section>
+  </body>
+</html>
+
+
+          
+    <!-- end -->
+      </div>
+    
     </div>
   </div>
 </div>
+<!-- signup modal -->
+<div class="modal fade" id="signup" tabindex="-1" aria-labelledby="exampleModalCenteredScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+     
+      
+
+
+    <div class="form signup">
+        <header>Kwugurura Konte</header>
+        <form action="#">
+          <input type="text" placeholder="Amazina Yose" required /><br>
+          <input type="text" placeholder="Shiramwo Email Yawe" required /><br>
+          <input type="password" placeholder="Shiramwo Kabanga" required /><br>
+          <input type="password" placeholder="Emeza Kabanga" required /><br>
+          <div class="checkbox">
+            <input type="checkbox" id="signupCheck" />
+            <label for="signupCheck">Emera Amabwiriza Hama Wemeze</label>
+          </div>
+          <input type="submit" value="Emeza" />
+        </form>
+      </div>
+
+
+          
+    <!-- end -->
+      </div>
+    </div>
+  </div>
+ </div>
+
+
+
+</section>
+
+<style>
+ 
+</style>
+
+
+
+
